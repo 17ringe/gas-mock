84321a5b07000c4a61a5351a8918233a3b149585
Kazuhiro Inari <inarinlab@gmail.com>
2018-05-14 08:10:44 +0900

    UrlFetchApp クラスのテストなどを追加

--- class/UrlFetchApp.js
+++ class/UrlFetchApp.js
@@ -2,30 +2,33 @@ var request = require('sync-request');
 
 // UrlFetchApp クラスのモックを作成
 function get_mock(param) {
-  return (function() {
-    var response = param;
+  return {
+    enabled: true,
+    response: param,
 
-    return {
-      // fetch 関数の実装
-      fetch: function(url, params) {
-        var method = 'GET';
-        if(params == null) params = {};
-        if(params['method'] != null) {
-          method = params['method'].toUpperCase();
-        }
-        var req;
-        if(method == 'POST') {
-          req = request(method, url, {
-            json: params['payload']
-          });
-        } else {
-          req = request(method, url);
-        }
-        response.content = req.body;
-        return response;
+    // fetch 関数の実装
+    fetch: function(url, params) {
+      if(this.enabled == false) {
+        this.response.content = new Buffer('');
+        return this.response;
       }
+      var method = 'GET';
+      if(params == null) params = {};
+      if(params['method'] != null) {
+        method = params['method'].toUpperCase();
+      }
+      var req;
+      if(method == 'POST') {
+        req = request(method, url, {
+          json: params['payload']
+        });
+      } else {
+        req = request(method, url);
+      }
+      this.response.content = req.body;
+      return this.response;
     }
-  })();
+  }
 }
 
 module.exports = {get_mock};
---
+++ test/test-urlfetchapp.js
@@ -0,0 +1,35 @@
+var assert = require('assert');
+var gas = require('gas-local');
+var Sugar = require('sugar');
+
+var mock = require('../index.js');
+
+var mymock = mock.globalMockDefault;
+
+mymock.Logger.enabled = false;
+
+// ソースフォルダの指定はプロジェクトルートからの相対パス
+var glib = gas.require('./src', mymock);
+
+describe('urlfetchapp.js', function() {
+
+  before(function() {
+    glib = mock.require('./src', mymock);
+  });
+
+  it('登録した関数が存在すること', function() {
+    assert.equal(glib.UrlFetchApp.hasOwnProperty('fetch'), true);
+  });
+
+  it('myUrlFetchApp for yahoo', function() {
+    assert.equal(glib.myUrlFetchApp(), 'Yahoo! JAPAN');
+  });
+
+  it('myUrlFetchApp for livedoor', function() {
+    assert.equal(glib.myUrlFetchApp('http://www.livedoor.com/'), 'livedoor');
+  });
+
+  it('myUrlFetchApp for Shift_JIS', function() {
+    assert.equal(glib.myUrlFetchApp('http://abehiroshi.la.coocan.jp/top.htm', 'sjis'), '阿部 寛のホームページ');
+  });
+});
