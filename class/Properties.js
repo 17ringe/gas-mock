3f8f229f9f37132af2d25198d6283601c7335fbb
Kazuhiro Inari <inarinlab@gmail.com>
2018-05-16 09:23:57 +0900

    Properties クラスのリファクタリングとテストの追加

--- class/Properties.js
+++ class/Properties.js
@@ -3,37 +3,38 @@ var Sugar = require('sugar');
 // Properties クラスのモックを作成
 var Properties = function() {
   var properties_ = {};
+  var properties = {};
 
-  return {
-    // getProperty 関数の実装
-    getProperty: function(key) {
-      var val = properties_[key];
-      return val != null ? val : null;
-    },
-
-    // setProperty 関数の実装
-    setProperty: function(key, value) {
-      Sugar.Object.set(properties_, key, value);
-      return properties_;
-    },
-
-    // getProperties 関数の実装
-    getProperties: function() {
-      return properties_;
-    },
-
-    // setProperties 関数の実装
-    setProperties: function(properties, deleteAllOthers) {
-      if(deleteAllOthers == true) deleteAllProperties();
-      Sugar.Object.merge(properties_, properties);
-      return properties_;
-    },
-
-    // deleteAllProperties 関数の実装
-    deleteAllProperties: function() {
-      properties_ = {};
-    }
+  // getProperty 関数の実装
+  properties.getProperty = function(key) {
+    var val = properties_[key];
+    return val != null ? val : null;
   }
+
+  // setProperty 関数の実装
+  properties.setProperty = function(key, value) {
+    Sugar.Object.set(properties_, key, value);
+    return properties_;
+  }
+
+  // getProperties 関数の実装
+  properties.getProperties = function() {
+    return properties_;
+  }
+
+  // setProperties 関数の実装
+  properties.setProperties = function(properties, deleteAllOthers) {
+    if(deleteAllOthers == true) deleteAllProperties();
+    Sugar.Object.merge(properties_, properties);
+    return properties_;
+  }
+
+  // deleteAllProperties 関数の実装
+  properties.deleteAllProperties = function() {
+    properties_ = {};
+  }
+
+  return properties;
 }
 
 function get_mock() {
--- test/test-properties.js
+++ test/test-properties.js
@@ -1,15 +1,28 @@
 var assert = require('chai').assert;
 var gas = require('gas-local');
-var Sugar = require('sugar');
 
 var mock = require('../index.js');
+var glib = gas.require('./src', mock.globalMockDefault);
 
-var mymock = mock.globalMockDefault;
+describe('Properties.js', function() {
 
-// ソースフォルダの指定はプロジェクトルートからの相対パス
-var glib = gas.require('./src', mymock);
+  it('ローカル変数に外部からアクセスできないこと', function() {
+    assert.isUndefined(glib.Properties.properties_);
+    assert.isUndefined(glib.Properties.properties);
+  });
 
-describe('properties.js', function() {
+  it('必要な関数が存在すること', function() {
+    assert.property(glib.Properties, 'getProperty');
+    assert.property(glib.Properties, 'getProperties');
+    assert.property(glib.Properties, 'setProperty');
+    assert.property(glib.Properties, 'setProperties');
+    assert.property(glib.Properties, 'deleteAllProperties');
+  });
+
+  xit('残2) 未実装の関数 http://bit.ly/2wHLPJz', function() {
+    assert.property(glib.Properties, 'deleteProperty');
+    assert.property(glib.Properties, 'getKeys');
+  });
 
   it('#setProperty', function() {
     assert.property(glib.testSPsetProperty('gas', 'ok'), 'gas');
